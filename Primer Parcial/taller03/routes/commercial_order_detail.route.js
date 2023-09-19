62a894a1830e17514c1b28397e32171982a55d0f
WinterMeza <e1317233748@live.uleam.edu.ec>
2023-09-19 15:00:24 -0500

    x

--- Primer Parcial/taller03/routes/commercial_order_detail.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commercialOrderDetailController = require("../controllers/commercial_order_detail.controllers");
-
-router.post("/", commercialOrderDetailController.createCommercialOrderDetail);
-
-router.get("/", commercialOrderDetailController.getCommercialOrderDetail);
-
-module.exports = router;
--- Primer Parcial/taller03/routes/supply_required.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const supplyRequiredController = require("../controllers/supply_required.controller");
-
-router.post("/", supplyRequiredController.createSupplyRequired);
-
-router.get("/", supplyRequiredController.getSupplyRequired);
-
-module.exports = router;
