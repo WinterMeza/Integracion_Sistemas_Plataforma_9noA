4fd741fe5c01f3b171eb928dd8806f163c99ea0e
WinterMeza <e1317233748@live.uleam.edu.ec>
2023-09-19 17:08:18 -0500

    TALLER 03 19/9/2023

--- Primer Parcial/taller03/routes/commercial_order.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commercialOrderController = require("../controllers/commercial_order.controller");
-
-router.post("/", commercialOrderController.createCommercialOrder);
-
-router.get("/", commercialOrderController.getCommercialOrder);
-
-module.exports = router;
