2cf2b889bfbf44f30e3da2e53a31ec7e12131434
WinterMeza <e1317233748@live.uleam.edu.ec>
2023-09-19 23:50:40 -0500

    TALLER 03 19/09/2023

--- Primer Parcial/taller03/test/app.test.js
+++ Primer Parcial/taller03/test/app.test.js
@@ -1,54 +1,68 @@
-const env_variables = require("dotenv").config();
-env_variables;
+const env_variables = require('dotenv').config()
+env_variables
 
-const request = require("supertest");
-const mongoose = require("mongoose");
+const request = require('supertest')
+const mongoose = require('mongoose')
 
-const app = require("../app");
-//const productModel = require("../models/product.model");
-const clienteModel = require("../models/cliente.model");
+const app = require('../app')
+const clienteModel = require('../models/cliente.model')
 
-describe("TESTING APP", () => {
-  beforeAll(async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-  });
+describe('TESTING APP', () => {
+    beforeAll(async () => {
+        await mongoose.connect(process.env.MONGO_URI)
+    })
 
-  afterAll(async () => await mongoose.disconnect());
+    afterAll(async () => await mongoose.disconnect())
 
-  describe("/GET testing", () => {
-    test("Responde con stado 200 ok", async () => {
-      const response = await request(app).get("/api/v1/client").send();
-      expect(response.status).toBe(200);
-    });
+    describe('/GET testing', () => {
+        test('Debería responder con estado 200 ok', async () => {
+            // Arrange: Configurar el estado inicial si es necesario
 
-    test("Responde con data ok", async () => {
-      const response = await request(app).get("/api/v1/client").send();
-      expect(response.body).toBeInstanceOf(Object);
-    });
-  });
+            // Act: Realizar la acción que se está probando
+            const response = await request(app)
+                .get('/api/v1/commercial-order-detail')
+                .send()
 
-  describe("/POST testing", () => {
-    afterAll(async () => {
-      await clienteModel.deleteMany({ ci_client: "6f8a1e2b9dc35b001c6c1e35" });
-    });
+            // Assert: Comprobar los resultados y expectativas
+            expect(response.status).toBe(200)
+            expect(response.body).toBeInstanceOf(Object)
+        })
+    })
 
-    const data = {
-      name_client: "Caroline Lucas",
-      direction: "A. Azul EFT Pradera 2",
-      phone: 994137994,
-      id_city: "Manta",
-      id_client_type: "5f8a1e2b9df35b001c6c2e29",
-      ci_client: "6f8a1e2b9dc35b001c6c1e35",
-    };
+    describe('/POST testing', () => {
+        afterEach(async () => {
+            await clienteModel.deleteMany({
+                ci_client: '6f8a1e2b9dc35b001c6c1e35',
+            })
+        })
 
-    test("Responde con stado 201 ok", async () => {
-      const response = await request(app).post("/api/v1/client").send();
-      expect(response.status).toBe(201);
-    });
+        test('Debería responder con estado 201 ok', async () => {
+            // Arrange: Configurar el estado inicial si es necesario
+            const data = {
+                proforma: '182-2023',
+                id_product: '6f8a1e2b9dc35b001c6c1e35',
+                id_tag: '5f8a1e2b9df35b001c6c2e29',
+                container_number: 2402,
+                box_number: 24,
+                box_amount: 50,
+                id_container_size: '8f8a1e2b9df35b001c6c2e29',
+                id_lid_type: '7f8a1e2b9df35b001c6c2e29',
+                net_weight: 5,
+                drained_weight: 10,
+                oil: 10,
+                water: 20,
+                loins: 50,
+                crumbs: 100,
+            }
 
-    test("Responde con datos insertados", async () => {
-      const response = await request(app).post("/api/v1/client").send(data);
-      expect(response.body.client._id).toBeDefined();
-    });
-  });
-});
+            // Act: Realizar la acción que se está probando
+            const response = await request(app)
+                .post('/api/v1/commercial-order-detail')
+                .send(data)
+
+            // Assert: Comprobar los resultados y expectativas
+            expect(response.status).toBe(201)
+            expect(response.body.commercialOrderDetail._id).toBeDefined()
+        })
+    })
+})
